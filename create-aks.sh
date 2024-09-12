92211aba440ab2ac489079494d94279bbb047ac1
Ovidiu Loghin <ovidiu.loghin@gmail.com>
2024-09-13 00:33:36 +0200

    francecentral

--- create-aks.sh
+++ create-aks.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-az group create --name MyResourceGroup --location eastus
-az aks create --resource-group MyResourceGroup --name MyAKSCluster --node-count 1 --enable-addons monitoring --generate-ssh-keys
+az group create --name aks-github-grp --location francecentral
+az aks create --resource-group aks-github-grp --name aks-scripted-cluster --node-count 1 --node-vm-size Standard_B2s --enable-addons monitoring --generate-ssh-keys --location fracecentral

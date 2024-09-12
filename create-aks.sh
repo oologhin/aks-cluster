aa2e7668d818c19f1e1782ca30415aa8e3f82ea5
Ovidiu Loghin <ovidiu.loghin@gmail.com>
2024-09-13 00:54:38 +0200

    nginx

--- create-aks.sh
+++ create-aks.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 az group create --name aks-github-grp --location francecentral
 az aks create --resource-group aks-github-grp --name aks-scripted-cluster --node-count 1 --node-vm-size Standard_B2s --enable-addons monitoring --generate-ssh-keys --location francecentral
+
